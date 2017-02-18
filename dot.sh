330f99c1ece6a1edfb06ea4d905dfcef2607ce2a
YF-Tung <matlov1024@gmail.com>
2017-02-18 19:10:15 +0800

    Fix

--- dot.sh
+++ dot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 temp=`mktemp -d` && cd $temp
-curl -O http://YF-Tung.github.io/dot.tar
+#curl -O http://YF-Tung.github.io/dot.tar
+curl -O http://YF-Tung.github.io/dot.9cd1738.tar
 tar xf dot.tar
 dot/install.sh
 cd ..
