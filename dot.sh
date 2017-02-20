28a36b1d546c2a74287d52c72bdae7f07148c1bf
Yu-Fan Tung <yftung@visualon.com>
2017-02-20 11:51:55 +0800

    Fix dot.sh

--- dot.sh
+++ dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 temp=`mktemp -d` && cd $temp
 #curl -O http://YF-Tung.github.io/dot.tar
-curl -O http://YF-Tung.github.io/dot.3010f6b.tar
+curl http://YF-Tung.github.io/dot.3010f6b.tar -o dot.tar
 tar xf dot.tar
 dot/install.sh
 cd ..
